c1c82198f6b1b0d983af7828dfccdc49cc8ad53c
Amogh Gupta <gupta.amogh15@gmail.com>
2018-08-22 20:13:58 +0530

    Added class properties support for es6 with babel plugin

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 // addOption component
 export default class AddOption extends React.Component {   
+    
+    // commented constructor once we installed the class properties in babel
+    // constructor(props){
+    //     super(props);
+    //     this.onFormSubmit = this.onFormSubmit.bind(this);
+    // }
 
-    constructor(props){
-        super(props);
-        this.onFormSubmit = this.onFormSubmit.bind(this);
-    }
-
-    onFormSubmit(e){
+    onFormSubmit= (e)=>{
         this.props.onFormSubmit(e, "data from child");
     }
 
